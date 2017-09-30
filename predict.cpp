c1a5c7db55dac3670f4c159b52e7a5a0bce1688a
Abdul Dakkak <abduld@wolfram.com>
2017-09-30 15:05:22 -0500

    add build_caffe2.sh

--- predict.cpp
+++ predict.cpp
@@ -29,7 +29,8 @@ using json = nlohmann::json;
 /* Pair (label, confidence) representing a prediction. */
 using Prediction = std::pair<int, float>;
 
-struct PredictorObject {
+struct PredictorObject
+{
   using Context = CUDAContext;
   PredictorObject(Predictor<Context> *const ctx) : ctx_(ctx){};
 
@@ -41,17 +42,21 @@ struct PredictorObject {
   profile *prof_{nullptr};
 };
 
-static void SetCUDA() {
-DeviceOption option;
-option.set_device_type(CUDA);
-new CUDAContext(option);
+static void SetCUDA()
+{
+  DeviceOption option;
+  option.set_device_type(CUDA);
+  new CUDAContext(option);
 }
 
-static void delete_prof(profile **prof) {
-  if (prof == nullptr) {
+static void delete_prof(profile **prof)
+{
+  if (prof == nullptr)
+  {
     return;
   }
-  if (*prof == nullptr) {
+  if (*prof == nullptr)
+  {
     return;
   }
   auto p = *prof;
@@ -61,8 +66,9 @@ static void delete_prof(profile **prof) {
 }
 
 template <class T>
-class TimeObserver final : public ObserverBase<T> {
- public:
+class TimeObserver final : public ObserverBase<T>
+{
+public:
   explicit TimeObserver<T>(T *subject, profile **prof,
                            std::string profile_name = "",
                            std::string profile_metadata = "")
@@ -72,7 +78,7 @@ class TimeObserver final : public ObserverBase<T> {
         profile_metadata_(profile_metadata) {}
   ~TimeObserver() {}
 
- private:
+private:
   profile **prof_{nullptr};
   profile_entry *entry_{nullptr};
   std::string profile_name_{""}, profile_metadata_{""};
@@ -81,14 +87,17 @@ class TimeObserver final : public ObserverBase<T> {
 };
 
 template <>
-bool TimeObserver<NetBase>::Start() {
+bool TimeObserver<NetBase>::Start()
+{
   const auto net = this->subject();
   auto net_name = net->Name();
-  if (net_name.empty()) {
+  if (net_name.empty())
+  {
     net_name = profile_name_;
   }
   *this->prof_ = new profile(net_name, profile_metadata_);
-  for (auto *op : subject_->GetOperators()) {
+  for (auto *op : subject_->GetOperators())
+  {
     op->SetObserver(caffe2::make_unique<TimeObserver<OperatorBase>>(op, prof_));
   }
   const auto p = *this->prof_;
@@ -97,17 +106,20 @@ bool TimeObserver<NetBase>::Start() {
 }
 
 template <>
-bool TimeObserver<NetBase>::Stop() {
+bool TimeObserver<NetBase>::Stop()
+{
   const auto p = *this->prof_;
   p->end();
   return true;
 }
 
 template <>
-bool TimeObserver<OperatorBase>::Start() {
+bool TimeObserver<OperatorBase>::Start()
+{
   const auto &op = this->subject();
   std::string name{""}, metadata{""};
-  if (op->has_debug_def()) {
+  if (op->has_debug_def())
+  {
     const auto &opdef = op->debug_def();
     name = opdef.type();
     metadata = opdef.name();
@@ -117,39 +129,47 @@ bool TimeObserver<OperatorBase>::Start() {
 }
 
 template <>
-bool TimeObserver<OperatorBase>::Stop() {
+bool TimeObserver<OperatorBase>::Stop()
+{
   this->entry_->end();
   const auto p = *this->prof_;
   p->add(this->entry_);
   return true;
 }
 
-PredictorContext New(char *predict_net_file, char *init_net_file) {
-  try {
-SetCUDA();
+PredictorContext New(char *predict_net_file, char *init_net_file)
+{
+  try
+  {
+    Workspace workspace;
+    SetCUDA();
     NetDef init_net, predict_net;
     CAFFE_ENFORCE(ReadProtoFromFile(init_net_file, &init_net));
     CAFFE_ENFORCE(ReadProtoFromFile(predict_net_file, &predict_net));
-       init_net.mutable_device_option()->set_device_type(CUDA);
-     predict_net.mutable_device_option()->set_device_type(CUDA);
-    const auto ctx = new Predictor<CUDAContext>(init_net, predict_net);
+    init_net.mutable_device_option()->set_device_type(CUDA);
+    predict_net.mutable_device_option()->set_device_type(CUDA);
+    const auto ctx = new Predictor<CUDAContext>(init_net, predict_net, &workspace);
     auto p = new PredictorObject(ctx);
     return (PredictorContext)p;
-  } catch (const std::invalid_argument &ex) {
+  }
+  catch (const std::invalid_argument &ex)
+  {
     LOG(ERROR) << "exception: " << ex.what();
     errno = EINVAL;
     return nullptr;
   }
-catch( const std::exception &e) {
+  catch (const std::exception &e)
+  {
     LOG(ERROR) << "exception: " << e.what();
     std::cout << "exception: " << e.what() << "\n";
     errno = EINVAL;
     return nullptr;
-}
+  }
 }
 
 const char *Predict(PredictorContext pred0, float *imageData, const int batch,
-                    const int channels, const int width, const int height) {
+                    const int channels, const int width, const int height)
+{
   auto obj = (PredictorObject *)pred0;
 
   const auto image_size = batch * channels * width * height;
@@ -168,16 +188,19 @@ const char *Predict(PredictorContext pred0, float *imageData, const int batch,
   auto ws = predictor->ws();
   auto net_def = predictor->def();
   auto net = ws->GetNet(net_def.name());
-  if (obj->profile_enabled_) {
+  if (obj->profile_enabled_)
+  {
     unique_ptr<TimeObserver<NetBase>> net_ob =
         make_unique<TimeObserver<NetBase>>(net, &obj->prof_, obj->profile_name_,
                                            obj->profile_metadata_);
     net->SetObserver(std::move(net_ob));
-  } else {
+  }
+  else
+  {
     net->RemoveObserver();
   }
 
-    net_def.mutable_device_option()->set_device_type(CUDA);
+  net_def.mutable_device_option()->set_device_type(CUDA);
   predictor->run(inputVec, &outputVec);
   auto &output = *(outputVec[0]);
   const auto len = output.size() / batch;
@@ -185,14 +208,17 @@ const char *Predict(PredictorContext pred0, float *imageData, const int batch,
 
   std::vector<Prediction> predictions;
   predictions.reserve(output.size());
-  for (int cnt = 0; cnt < batch; cnt++) {
-    for (int idx = 0; idx < len; idx++) {
+  for (int cnt = 0; cnt < batch; cnt++)
+  {
+    for (int idx = 0; idx < len; idx++)
+    {
       predictions.emplace_back(std::make_pair(idx, probs[cnt * len + idx]));
     }
   }
 
   json preds = json::array();
-  for (const auto prediction : predictions) {
+  for (const auto prediction : predictions)
+  {
     preds.push_back(
         {{"index", prediction.first}, {"probability", prediction.second}});
   }
@@ -200,30 +226,36 @@ const char *Predict(PredictorContext pred0, float *imageData, const int batch,
   return res;
 }
 
-void Delete(PredictorContext pred) {
+void Delete(PredictorContext pred)
+{
   auto predictor = (PredictorObject *)pred;
-  if (predictor) {
+  if (predictor)
+  {
     delete_prof(&predictor->prof_);
     delete predictor;
   }
 }
 
-void Init() {
+void Init()
+{
   int dummy_argc = 1;
   const char *dummy_name = "go-caffe2";
   char **dummy_argv = const_cast<char **>(&dummy_name);
   GlobalInit(&dummy_argc, &dummy_argv);
 
-SetCUDA();
+  SetCUDA();
 }
 
 void StartProfiling(PredictorContext pred, const char *name,
-                    const char *metadata) {
+                    const char *metadata)
+{
   auto predictor = (PredictorObject *)pred;
-  if (name == nullptr) {
+  if (name == nullptr)
+  {
     name = "";
   }
-  if (metadata == nullptr) {
+  if (metadata == nullptr)
+  {
     metadata = "";
   }
   predictor->profile_enabled_ = true;
@@ -231,21 +263,25 @@ void StartProfiling(PredictorContext pred, const char *name,
   predictor->profile_metadata_ = std::string(metadata);
 }
 
-void EndProfiling(PredictorContext pred) {
+void EndProfiling(PredictorContext pred)
+{
   auto predictor = (PredictorObject *)pred;
-  if (predictor->prof_) {
+  if (predictor->prof_)
+  {
     predictor->prof_->end();
   }
 }
 
-void DisableProfiling(PredictorContext pred) {
+void DisableProfiling(PredictorContext pred)
+{
   auto predictor = (PredictorObject *)pred;
   delete_prof(&predictor->prof_);
   predictor->profile_name_ = std::string("");
   predictor->profile_metadata_ = std::string("");
 }
 
-char *ReadProfile(PredictorContext pred) {
+char *ReadProfile(PredictorContext pred)
+{
   auto predictor = (PredictorObject *)pred;
   const auto s = predictor->prof_->read();
   const auto cstr = s.c_str();
