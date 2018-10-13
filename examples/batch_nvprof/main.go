9b8b4453ec4c044e3a605ae7f64190bd92199782
Cheng Li <cli99@illinois.edu>
2018-10-13 18:34:45 -0500

    work

---
+++ examples/batch_nvprof/main.go
@@ -0,0 +1,196 @@
+package main
+
+// #cgo linux CFLAGS: -I/usr/local/cuda/include
+// #cgo linux LDFLAGS: -lcuda -lcudart -L/usr/local/cuda/lib64
+// #include <cuda.h>
+// #include <cuda_runtime.h>
+// #include <cuda_profiler_api.h>
+import "C"
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"image"
+	"os"
+	"path/filepath"
+
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+	"github.com/k0kubun/pp"
+	"github.com/rai-project/config"
+	"github.com/rai-project/dlframework/framework/options"
+	"github.com/rai-project/downloadmanager"
+	caffe2 "github.com/rai-project/go-caffe2"
+	nvidiasmi "github.com/rai-project/nvidia-smi"
+	"github.com/rai-project/tracer"
+
+	"github.com/rai-project/tracer/ctimer"
+	_ "github.com/rai-project/tracer/jaeger"
+)
+
+var (
+	batchSize    = 64
+	model        = "bvlc_alexnet"
+	graph_url    = "http://s3.amazonaws.com/store.carml.org/models/caffe2/bvlc_alexnet_1.0/predict_net.pb"
+	weights_url  = "http://s3.amazonaws.com/store.carml.org/models/caffe2/bvlc_alexnet_1.0/init_net.pb"
+	features_url = "http://data.dmlc.ml/mxnet/models/imagenet/synset.txt"
+)
+
+// convert go Image to 1-dim array
+func cvtImageTo1DArray(src image.Image, mean []float32) ([]float32, error) {
+	if src == nil {
+		return nil, fmt.Errorf("src image nil")
+	}
+
+	b := src.Bounds()
+	h := b.Max.Y - b.Min.Y // image height
+	w := b.Max.X - b.Min.X // image width
+
+	res := make([]float32, 3*h*w)
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			r, g, b, _ := src.At(x+b.Min.X, y+b.Min.Y).RGBA()
+			res[y*w+x] = float32(b>>8) - mean[0]
+			res[w*h+y*w+x] = float32(g>>8) - mean[1]
+			res[2*w*h+y*w+x] = float32(r>>8) - mean[2]
+		}
+	}
+
+	return res, nil
+}
+
+func main() {
+	defer tracer.Close()
+
+	dir, _ := filepath.Abs("../tmp")
+	dir = filepath.Join(dir, model)
+	graph := filepath.Join(dir, "predict_net.pb")
+	weights := filepath.Join(dir, "init_net.pb")
+	features := filepath.Join(dir, "synset.txt")
+
+	defer tracer.Close()
+
+	if _, err := os.Stat(graph); os.IsNotExist(err) {
+		if _, err := downloadmanager.DownloadInto(graph_url, dir); err != nil {
+			panic(err)
+		}
+	}
+	if _, err := os.Stat(weights); os.IsNotExist(err) {
+
+		if _, err := downloadmanager.DownloadInto(weights_url, dir); err != nil {
+			panic(err)
+		}
+	}
+	if _, err := os.Stat(features); os.IsNotExist(err) {
+
+		if _, err := downloadmanager.DownloadInto(features_url, dir); err != nil {
+			panic(err)
+		}
+	}
+
+	imgDir, _ := filepath.Abs("../_fixtures")
+	imagePath := filepath.Join(imgDir, "platypus.jpg")
+
+	img, err := imgio.Open(imagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	var input []float32
+	for ii := 0; ii < batchSize; ii++ {
+		resized := transform.Resize(img, 227, 227, transform.Linear)
+		res, err := cvtImageTo1DArray(resized, []float32{123, 117, 104})
+		if err != nil {
+			panic(err)
+		}
+		input = append(input, res...)
+	}
+
+	opts := options.New()
+
+	device := options.CPU_DEVICE
+	if nvidiasmi.HasGPU {
+		device = options.CUDA_DEVICE
+	}
+
+	ctx := context.Background()
+
+	span, ctx := tracer.StartSpanFromContext(ctx, tracer.FULL_TRACE, "caffe2_batch")
+	defer span.Finish()
+
+	predictor, err := caffe2.New(
+		options.WithOptions(opts),
+		options.Device(device, 0),
+		options.Graph([]byte(graph)),
+		options.Weights([]byte(weights)),
+		options.BatchSize(batchSize))
+	if err != nil {
+		panic(err)
+	}
+	defer predictor.Close()
+
+	err = predictor.Predict(ctx, input)
+	if err != nil {
+		panic(err)
+	}
+	predictor.StartProfiling("predict", "")
+
+	err = predictor.Predict(input, 3, 227, 227)
+	if err != nil {
+		panic(err)
+	}
+
+	predictor.EndProfiling()
+
+	if nvidiasmi.HasGPU {
+		cu.Wait()
+		cu.Close()
+	}
+
+	profBuffer, err := predictor.ReadProfile()
+	if err != nil {
+		panic(err)
+	}
+	predictor.DisableProfiling()
+
+	t, err := ctimer.New(profBuffer)
+	if err != nil {
+		panic(err)
+	}
+	t.Publish(ctx)
+
+	predictions := predictor.ReadPredictedFeatures(ctx)
+
+	if true {
+		var labels []string
+		f, err := os.Open(features)
+		if err != nil {
+			os.Exit(-1)
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line := scanner.Text()
+			labels = append(labels, line)
+		}
+
+		len := len(predictions) / batchSize
+		for i := 0; i < 1; i++ {
+			res := predictions[i*len : (i+1)*len]
+			res.Sort()
+			pp.Println(res[0].Probability)
+			pp.Println(labels[res[0].Index])
+		}
+	} else {
+		_ = predictions
+	}
+}
+
+func init() {
+	config.Init(
+		config.AppName("carml"),
+		config.VerboseMode(true),
+		config.DebugMode(true),
+	)
+}
