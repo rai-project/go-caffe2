dc65e25c518741e8778d44f9265227799e230ce5
Cheng Li <cli99@illinois.edu>
2018-10-14 03:03:43 -0500

    work

--- cbits/predictor.hpp
+++ cbits/predictor.hpp
@@ -11,31 +11,28 @@ typedef void *PredictorContext;
 
 typedef enum { CPU_DEVICE_KIND = 0, CUDA_DEVICE_KIND = 1 } DeviceKind;
 
-PredictorContext NewCaffe2(char *predict_net_file, char *init_net_file,
+PredictorContext NewCaffe2(char *init_net_file, char *net_file,
                            DeviceKind device);
-
-// int InitCaffe2(DeviceKind device);
 
 void InitCaffe2(DeviceKind device_kind);
 
 void PredictCaffe2(PredictorContext pred, float *imageData, const int batch,
-                   const int channels, const int width, const int height,
-                   DeviceKind device);
+                   const int channels, const int width, const int height);
 
-const float *GetPredictionsCaffe2(PredictorContext pred, DeviceKind device);
+const float *GetPredictionsCaffe2(PredictorContext pred);
 
-void DeleteCaffe2(PredictorContext pred, DeviceKind device);
+void DeleteCaffe2(PredictorContext pred);
 
 void StartProfilingCaffe2(PredictorContext pred, const char *name,
-                          const char *metadata, DeviceKind device);
+                          const char *metadata);
 
-void EndProfilingCaffe2(PredictorContext pred, DeviceKind device);
+void EndProfilingCaffe2(PredictorContext pred);
 
-void DisableProfilingCaffe2(PredictorContext pred, DeviceKind device);
+void DisableProfilingCaffe2(PredictorContext pred);
 
-char *ReadProfileCaffe2(PredictorContext pred, DeviceKind device);
+char *ReadProfileCaffe2(PredictorContext pred);
 
-int GetPredLenCaffe2(PredictorContext pred, DeviceKind device);
+int GetPredLenCaffe2(PredictorContext pred);
 
 #ifdef __cplusplus
 }
