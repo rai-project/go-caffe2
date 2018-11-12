7c85ed9280543a7616f11d460c0a50614abc7b7a
Abdul Dakkak <dakkak@illinois.edu>
2018-11-12 20:13:02 +0000

    update build

--- scripts/build_caffe2.sh
+++ scripts/build_caffe2.sh
@@ -1,7 +1,7 @@
 FRAMEWORK_VERSION=v0.4.1
 CAFFE2_SRC_DIR=$HOME/code/pytorch
 
-git clone --single-branch --depth=1 --recurse-submodules -j8 --branch=$FRAMEWORK_VERSION https://github.com/pytorch/pytorch.git $CAFFE2_SRC_DIR
+git clone --single-branch --depth=1 --recurse-submodules --branch=$FRAMEWORK_VERSION https://github.com/pytorch/pytorch.git $CAFFE2_SRC_DIR
 
 CAFFE2_DIST_DIR=/opt/pytorch/caffe2
 mkdir -p $CAFFE2_DIST_DIR
@@ -16,6 +16,7 @@ cd $CAFFE2_SRC_DIR && git submodule update --init && mkdir -p build && cd build 
       -DUSE_NNPACK=OFF \
       -DUSE_ROCKSDB=OFF \
       -DBUILD_PYTHON=OFF \
+      -DBUILD_BINARY=OFF \
       -DUSE_OPENCV=OFF \
       -DUSE_NNPACK=OFF \
       -DUSE_GLOO=OFF \
